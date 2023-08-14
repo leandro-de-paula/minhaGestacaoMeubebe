e8853dfe299788280762d1b63c27edfaed23cc90
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-08-14 15:48:12 -0300

    Update Project - Reorganization of folders and inclusion of pages by JS

---
+++ main.js
@@ -0,0 +1,16 @@
+// Função para carregar e exibir o conteúdo de uma página
+function carregarConteudo(elementId, url) {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro:', error);
+      });
+  }
+  
+  // Carregar os diferentes conteúdos nas áreas apropriadas
+  carregarConteudo('header', '../pages/bodyHeader.html');
+  carregarConteudo('footer', '../pages/bodyFooter.html');
+  
