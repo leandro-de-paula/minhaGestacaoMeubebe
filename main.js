c39382e650db4ca865f0825aebd9a36673025ec8
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-08-14 18:06:37 -0300

    Update 404

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Função para carregar e exibir o conteúdo de uma página
 function carregarConteudo(elementId, url) {
-    fetch(url)
+    // fetch(url)
+    fetch(`${BASE_PATH}${url}`)
       .then(response => response.text())
       .then(data => {
         document.getElementById(elementId).innerHTML = data;
@@ -11,6 +12,6 @@ function carregarConteudo(elementId, url) {
   }
   
   // Carregar os diferentes conteúdos nas áreas apropriadas
-  carregarConteudo('header', '../pages/bodyHeader.html');
-  carregarConteudo('footer', '../pages/bodyFooter.html');
+  carregarConteudo('header', 'pages/bodyHeader.html');
+  carregarConteudo('footer', 'pages/bodyFooter.html');
   
