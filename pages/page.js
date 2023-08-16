27e032ab6adc7b1b5f96c30b536fa241a5c53bb0
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-08-16 21:17:43 +0000

    Test 404 - Step 02

--- pages/page.js
+++ pages/page.js
@@ -1,7 +1,9 @@
-document.getElementById('index').href = PAGE ? 'index.html' : '../index.html'
+document.getElementById('index').href = PAGE ? '../index.html' : 'index.html'
 document.getElementById('logo').src = PAGE ? '../assets/img/logo.png' : 'assets/img/logo.png'
+document.getElementById('home').href = PAGE ? '../index.html' : 'index.html'
 document.getElementById('calcularIdadeGestacional').href = PAGE ? 'calcularIdadeGestacional.html' : 'pages/calcularIdadeGestacional.html'
 
-console.log(document.getElementById('index').href)
+console.log(document.getElementById('logo').src)
+console.log(document.getElementById('home').href)
 
 
