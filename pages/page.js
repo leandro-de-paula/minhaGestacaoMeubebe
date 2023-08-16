fa7d30810381da8dcf7d030a50764a0634f7c176
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-08-16 20:58:13 +0000

    Test 404

--- pages/page.js
+++ pages/page.js
@@ -1,2 +1,7 @@
-document.getElementById('index').href = PAGE ? '../index.html' : 'index.html'
+document.getElementById('index').href = PAGE ? 'index.html' : '../index.html'
+document.getElementById('logo').src = PAGE ? '../assets/img/logo.png' : 'assets/img/logo.png'
 document.getElementById('calcularIdadeGestacional').href = PAGE ? 'calcularIdadeGestacional.html' : 'pages/calcularIdadeGestacional.html'
+
+console.log(document.getElementById('index').href)
+
+
