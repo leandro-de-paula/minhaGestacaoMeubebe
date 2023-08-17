3ae77963d40daf610c721480d9276b931bc5c11f
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-08-17 05:29:37 -0300

    Clear Code - 01

--- pages/page.js
+++ pages/page.js
@@ -1,9 +1,10 @@
+// Links Header
 document.getElementById('index').href = PAGE ? '../index.html' : 'index.html'
 document.getElementById('logo').src = PAGE ? '../assets/img/logo.png' : 'assets/img/logo.png'
 document.getElementById('home').href = PAGE ? '../index.html' : 'index.html'
 document.getElementById('calcularIdadeGestacional').href = PAGE ? 'calcularIdadeGestacional.html' : 'pages/calcularIdadeGestacional.html'
 
-console.log(document.getElementById('logo').src)
-console.log(document.getElementById('home').href)
+// Links Footer
+
 
 
